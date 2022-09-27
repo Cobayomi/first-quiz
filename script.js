5110090958331ed75d3a2bd972d967a73fdfeb22
capricia <capriciaw16@gmail.com>
2022-09-26 23:32:54 -0500

    push before submit

--- script.js
+++ script.js
@@ -3,3 +3,10 @@
 // 3. You can't select any option once time goes off.  
 // 3. You can't exit from the Quiz while you're playing.
 // 4. You'll get points on the basis of your correct answers.
+
+//Commonly used data types DO NOT INCLUDE:
+// strings
+// booleans
+// alerts
+// numbers
+
